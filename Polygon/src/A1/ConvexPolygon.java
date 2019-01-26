da753d573863aa43fb80bb323a409c4ccf46a0b9
KingKP07 <karmit1998@yahoo.com>
2019-01-25 19:24:19 -0500

    Convex method added.

--- Polygon/src/A1/ConvexPolygon.java
+++ Polygon/src/A1/ConvexPolygon.java
@@ -8,6 +8,8 @@
  **********************************************************/
 package A1;
 
+import java.awt.geom.Point2D;
+
 /**
  * The class ConvexPolygon extends SimplePolygon.
  * 
@@ -16,5 +18,48 @@ package A1;
  * @author Andy Mirzaian
  */
 public class ConvexPolygon extends SimplePolygon {
-	// TODO: add your code here
+
+	private Point2D.Double[] vert;
+	private double oSlope, nSlope;
+	private int size, dir;
+	private boolean convex = true;
+
+	public boolean isConvex() {
+		SimplePolygon poly = new SimplePolygon().getNewPoly();
+		vert = poly.vertices;
+		size = poly.getSize();
+		dir = 1;
+
+		oSlope = (vertices[1].getY() - vertices[0].getY()) / (vertices[1].getX() - vertices[0].getX());
+
+		for (int i = 1; i < n; i++) {
+			Point2D.Double p1 = vertices[i];
+			Point2D.Double p2;
+			if (i == n - 1)
+				p2 = vertices[0];
+			else
+				p2 = vertices[n + 1];
+
+			nSlope = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+
+			if (i == 1) {
+				if (nSlope <= oSlope) {
+					dir = 1;
+				} else {
+					dir = -1;
+				}
+			} else {
+				if (dir == 1 && nSlope >= oSlope) {
+					convex = false;
+					break;
+				} else if (dir == -1 && nSlope < oSlope) {
+					convex = false;
+					break;
+				}
+			}
+
+		}
+
+		return convex;
+	}
 }
